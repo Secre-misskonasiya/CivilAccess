19cd2d68e8c1f4da4872bc85b6e46248b6b1c80b
Cymieus <depechemode19y@gmail.com>
2026-07-30 19:01:24 +0800

    Progress report changes

---
+++ Web Capstone/Capstone/demo/src/main/resources/static/js/supabase-client.js
@@ -0,0 +1,6 @@
+if (!window.supabaseClient) {
+    window.supabaseClient = window.supabase.createClient(
+        'https://upfrofppponiqffbssqb.supabase.co',
+        'sb_publishable_tZyxrTJrpd_OD5-TKUkT8Q__DnuY0tj'
+    );
+}
